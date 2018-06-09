e1faa8b39e7523d638660a46cf45610236a90680
Grayback <35223206+GrayDevs@users.noreply.github.com>
2018-06-10 00:02:49 +0200

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -1,129 +1,137 @@
 #!/bin/bash
 
-courant="Exemple/Test/A"
-ARCHIVE="./Archive/test2.arch"
+# Description :
+# Ce script utilise les fichiers suivants :
+# /tmp/rep.txt
+# /tmp/test.txt
 
-grep '^directory' $ARCHIVE | awk '{print $2}' > rep.txt
+set -euo pipefail
 
+#### FONCTION
+
+# La fonction liste_fichier() permet de lister les fichier d'une répertoire
+# $1	ARCHIVE
+# $2	REPERTOIRE
 function listefich() 
 {
 	arch=$1
 	rep=$2
 
-	rm test.txt
-	touch test.txt
+	touch /tmp/test.txt
 	ligne=$(grep -n '^directory '$rep'' $arch | head -1 | cut -d: -f1)
 
 	lignedel=$(grep -n '^@$' $arch | cut -d: -f1)
 	lignedel=$(echo $lignedel | sed 's/ /:/g')
 	
-	n=$(grep -n '^'$rep'$' rep.txt | head -1 | cut -d: -f1)
+	n=$(grep -n '^'$rep'$' /tmp/rep.txt | head -1 | cut -d: -f1)
 
 	fin=$(echo $lignedel | cut -d: -f$n)
 
 	nbligne=$(($fin-$ligne-1))
-	echo $(cat $arch | head -$((fin-1)) | tail -$((nbligne))) > test.txt
+	echo $(cat $arch | head -$((fin-1)) | tail -$((nbligne))) > /tmp/test.txt
 
 }
 
-function testrep()
-{
+# La fonction testrep() test si un répertoire existe
+# $1	REPERTOIRE
+function test_rep() {
+
+	flag=0
 	testrep=$1
-	flag2=0
-	while read ligne
-	do
+	while read ligne; do
 		if [ "$ligne" = "$testrep" ];then
-			flag2=1
+			flag=1
 		fi
-	done < rep.txt
-	if [ $flag2 -eq 1 ];then
-		courant=$testrep
-	else 
-		echo "le répertoire n'existe pas"				
-	fi
+	done < /tmp/rep.txt
+	
+	if [ $flag -eq 1 ];then
+		CURRENT=$testrep
+		
+	elif [ $flag -eq 0 ];then
+		listefich $ARCHIVE $CURRENT
+		if [ -z  /tmp/test.txt ];then
+			echo "le répertoire est vide il n'y a pas de sous répertoire possible"
+			exit 1
+		fi
+		
+		for word in $(cat /tmp/test.txt); do
+			if [ "$word" = "$repertoire" ];then
 
+				while read ligne; do
+					if [ "$ligne" = "$CURRENT/$repertoire" ];then
+						flag=1
+					fi
+				done < /tmp/rep.txt
+
+			fi
+		done
+			
+		if [ $flag -eq 1 ];then
+			CURRENT="$CURRENT/$repertoire"
+		else
+			echo "ce n'est pas un sous répertoire"
+		fi
+
+	fi
 }
 
-if [ $# -eq 0 ];then
-	courant=$(cat rep.txt | head -1 | sed 's/\/$//g')
-
-elif [ $# -eq 1 ];then
-	repertoire=$1
-
-
-	if [ "$repertoire" = "/" ];then
-		courant=$(cat rep.txt | head -1 | sed 's/\/$//g')
-		echo $courant
-		exit 1
-	fi
-
-	test=$(echo $repertoire | awk -F/ '{print $1}')
+# La fonction change_directory effectue le changement de répertoire
+# $1	REPERTOIRE
+function change_directory() {
+	# On récupère une liste des répertoire de l'archive qui nous sera utile pour les autres commandes
+	grep '^directory' $ARCHIVE | sed 's/directory //g' > /tmp/rep.txt
 	
-	if [ "$test" = ".." ]; then
-		if [ "$repertoire" = ".." ];then
-		
-			courant=$(echo $courant | sed 's/\(.*\)\/[A-Z a-z 0-9]*$/\1/g')
+	#grep '^directory' $ARCHIVE | awk '{print $2}' > /tmp/rep.txt
 
-		else 
-			cible=$(echo $repertoire | sed 's/^\.\.\/\(.*\)$/\1/g')
-			pere=$(echo $courant | sed 's/\(.*\)\/[A-Z a-z 0-9]*$/\1/g')
-			rep="$pere/$cible"
-			testrep $rep
-		fi
-			
-			
-	elif [ "$test" = "." ];then
-		if [ "$repertoire" = "." ];then
-			echo "préciser un sous répertoire apres le . "
-		else 
-		
-			cible=$(echo $repertoire | sed 's/^.\/\(.*\)$/\1/g')
-			rep="$courant/$cible"
-			testrep $rep
-		fi
-	
-	else
-		flag=0
-		#testrep $cible
-		testrep=$repertoire
-		while read ligne
-		do
-			if [ "$ligne" = "$testrep" ];then
-				flag=1
-			fi
-		done < rep.txt
-		if [ $flag -eq 1 ];then
-			courant=$testrep
-		
-		elif [ $flag -eq 0 ];then
-			listefich $ARCHIVE $courant
-			if [ -z  test.txt ];then
-				echo "le répertoire est vide il n'y a pas de sous répertoire possible"
-				exit 1
-			fi
-			for word in $(cat test.txt)
-			do
-				if [ "$word" = "$repertoire" ];then
-					while read ligne
-					do
-						if [ "$ligne" = "$courant/$repertoire" ];then
-							flag=1
-						fi
-				
-					done < rep.txt
+	# Actions selon le nombre d'arguments
+
+	# si "cd" (retour à la racine)
+	if [ $# -eq 0 ]; then 
+		CURRENT=$RACINE
+
+	#si "cd <>" (1 argument)
+	elif [ $# -eq 1 ]; then
+
+		repertoire=$1 #cd <repertoire>
+
+		#si "cd /" (retour à la racine)
+		if [ "$repertoire" = "/" ]; then 
+			CURRENT=$RACINE
+		else
+			# récupère le nom du répertoire ou le champs précédant un éventuelle / 
+			test=$(echo $repertoire | awk -F/ '{print $1}')
+
+			#si "cd ..[/<...>]"
+			if [ "$test" = ".." ]; then
+				#si "cd ..", on remonte dans l'arborescence
+				if [ "$repertoire" = ".." ]; then
+					CURRENT=$(echo $CURRENT | sed 's/\(.*\)\/[A-Z a-z 0-9]*$/\1/g')
+				else #sinon ("cd ../<...>")
+					cible=$(echo $repertoire | sed 's/^\.\.\/\(.*\)$/\1/g')
+					pere=$(echo $CURRENT | sed 's/\(.*\)\/[A-Z a-z 0-9]*$/\1/g')
+					rep="$pere/$cible"
+					test_rep $rep #appel de la fonction test_rep()
 				fi
-			done
-			if [ $flag -eq 1 ];then
-				courant="$courant/$repertoire"
+			#si "cd .[/<...>]"
+			elif [ "$test" = "." ]; then
+				#si "cd ./<...>"
+				if [ "$repertoire" != "." ]; then
+					cible=$(echo $repertoire | sed 's/^.\/\(.*\)$/\1/g')
+					rep="$CURRENT/$cible"
+					test_rep $rep
+				fi
+			#sinon
 			else
-				echo "ce n'est pas un sous répertoire"
+				test_rep $repertoire
 			fi
-
 		fi
+	else # Nombre d'argument > 1
+		echo "il y a trop d'arguments" 
 	fi
-else 
-	echo "il y a trop d'arguments" 
-fi
 
-rm rep.txt
-echo $courant
+	rm /tmp/rep.txt #nettoyage
+	echo "CURRENT : $CURRENT"
+}
+
+##### PROCESS
+change_directory $arg
--- vsh-browse.sh
+++ vsh-browse.sh
@@ -13,7 +13,8 @@ fi
 
 # Variable(s)
 export ARCHIVE="Archives/$1"
-export CURRENT=$(sed -n '3p' $ARCHIVE | awk '{print $2}' | sed 's/\/$//g')
+export RACINE=$(sed -n '3p' $ARCHIVE | awk '{print $2}' | sed 's/\/$//g')
+export CURRENT=$RACINE
 
 #### FONCTION
 
@@ -26,9 +27,9 @@ function control() {
     cmd=$(echo $1 | awk '{print $1}')
     if [ $nb_arg -gt 2 ]; then
         cmd="erreur"
-    elif [ $nb_arg -ne 1 ] && ([ "$cmd" == "pwd" ] || [ "$cmd" == "help" ] || [ "$cmd" == "exit" ]); then
+    elif [ $nb_arg -ne 1 ] && ([ "$cmd" == "pwd" ] || [ "$cmd" == "clear" ] || [ "$cmd" == "help" ] || [ "$cmd" == "exit" ]); then
         cmd="erreur"    
-    elif [ $nb_arg -ne 2 ] && ([ "$cmd" == "cat" ] || [ "$cmd" == "cd" ] || [ "$cmd" == "rm" ]); then
+    elif [ $nb_arg -ne 2 ] && ([ "$cmd" == "cat" ] || [ "$cmd" == "rm" ]); then
         cmd="erreur"
     fi
 }
@@ -55,16 +56,16 @@ while [ "$is_running" == "TRUE" ]; do
                 ./ls.sh $arg
                 ;;
             "cat")
-                echo "cat "
                 ./cat.sh $arg
                 ;;
             "cd")
-                echo 
-                ./cd.sh $arg
+                . ./cd.sh #$arg
+                ;;
+            "clear")
+                clear
                 ;;
             "rm")
-                echo "rm"
-                ./rm.sh
+                ./rm.sh $arg
                 ;; 
             "help")
                 printf "Commandes possibles : pwd ; ls <directory> ; cd <directory> ;\n cat <file_name> ; rm <file_name> ; exit\n"
